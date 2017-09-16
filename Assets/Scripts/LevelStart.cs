b761634687279897071686163692e391d00bba28
hc09141 <hc09141@users.noreply.github.com>
2017-09-16 12:33:11 +1200

    Sound transitions between levels

---
+++ Assets/Scripts/LevelStart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelStart : MonoBehaviour {
+	public AudioSource StartSound;
+
+	void Awake() {
+		DontDestroyOnLoad (StartSound);
+	}
+
+	public void StartPressed() {
+		StartSound.Play ();
+		SceneManager.LoadScene ("LevelOne");
+	}
+}
