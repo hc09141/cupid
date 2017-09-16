c2ee854fb72f37c8bc798d280420ce30731da9a9
CDeighton <cullumdeighton@gmail.com>
2017-09-17 10:18:55 +1200

    Added basic player movement

---
+++ Assets/Scripts/Overlay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overlay : MonoBehaviour {
+	public static string HOBO = "hobo";
+	public static string PETITIONER = "petitioner";
+	public static string CELEBRITY = "celebrity";
+
+	public static Vector3 SPAWN_POINT = new Vector3 (0.0f, 0.0f, 0.0f);
+
+	public GameObject HOBO_PREFAB;
+
+	void Start()
+	{
+		HOBO_PREFAB.active = false;
+
+		Button[] buttons = this.GetComponentsInChildren<Button> ();
+
+		foreach (Button button in buttons) {
+			button.onClick.AddListener(delegate{AddCreep(button.name);});
+		}
+	}
+
+	void AddCreep(string CreepType) {
+		HOBO_PREFAB.active = true;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour {
+	public Vector3 goal;
+    NavMeshAgent agent;
+       
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+		goal = new Vector3 (0, 0, 0);
+		goal = agent.transform.position;
+    }
+
+    void Update() {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast (ray, out hit)) {
+				goal = NearestPointOnMesh(hit.point);
+			}
+		}
+
+		agent.destination = goal; 
+    }
+
+	Vector3 NearestPointOnMesh(Vector3 point) {
+		NavMeshHit hit;
+		if (NavMesh.SamplePosition(point, out hit, 1.0f, NavMesh.AllAreas)) {
+			return hit.position;
+			//Debug.DrawLine(transform.position, hit.position, Color.red, 5);
+		}
+		return point;
+	}
+}
