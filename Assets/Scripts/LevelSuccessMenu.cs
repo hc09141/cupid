08ea4d593ca7543221c76542e0660c016ffd553e
hc09141 <hc09141@users.noreply.github.com>
2017-09-17 13:36:03 +1200

    Success screens

---
+++ Assets/Scripts/LevelSuccessMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSuccessMenu : MonoBehaviour {
+	public string NextScene;
+	public AudioSource StartSound;
+
+	void Awake() {
+		DontDestroyOnLoad (StartSound);
+	}
+
+	public void StartPressed() {
+		StartSound.Play ();
+		SceneManager.LoadScene (NextScene);
+	}
+
+	public void QuitPressed() {
+		StartSound.Play ();
+		SceneManager.LoadScene ("StartMenu");
+	}
+}
